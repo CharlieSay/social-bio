ec6a9c5273642a00ae9ae86235b6d0d237e6ddc2
Charlie Say <charliewsay@gmail.com>
2023-01-26 20:56:22 +0000

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-
+import Image from 'next/image'
 export default function RootLayout({
   children,
 }: {
@@ -11,6 +11,18 @@ export default function RootLayout({
       <body className="text-center bg-slate-100 container max-w-2xl mx-auto px-4 md:px-32 lg:px-32">
         {children}
       </body>
+      <footer className="text-center mt-16 text-slate-500 container max-w-2xl mx-auto px-4 md:px-32 lg:px-32">
+        <section className="flex flex-col justify-center">
+          <p className="text-xs">Created with</p>
+          <Image
+            className="mx-auto mt-2 relative drop-shadow-2xl"
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={90}
+            height={18}
+          />
+        </section>
+      </footer>
     </html>
   )
 }
