320330ab8f51ba20f0f1d4c3600158caeb392189
Charlie Say <charliewsay@gmail.com>
2023-01-27 12:58:58 +0000

    probs fix zoom in problem

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,7 +2,10 @@ export default function Head() {
   return (
     <>
       <title>SocialBio | Generate your social media bios</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        content="width=device-width, initial-scale=1, maximum-scale=1"
+        name="viewport"
+      />
       <meta
         name="description"
         content="Created in NextJS, Leveraging openAI and vercel edge functions."
