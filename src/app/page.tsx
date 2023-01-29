4c8ffb5ab0755d472ca17519dd947d3cd318315a
Charlie Say <charliewsay@gmail.com>
2023-01-29 09:34:26 +0000

    dunno what im committing but it works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Form } from '@/components/form'
 export default function Home() {
   return (
     <main className="pt-24">
-      <h1 className="leading-none tracking-normal md:text-6xl md:tracking-tight block w-full py-2 animate-text bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-transparent text-5xl font-black lg:inline text-center">
+      <h1 className="leading-none tracking-normal md:text-6xl md:tracking-tight block w-full py-2 animate-text bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-transparent  text-5xl font-black lg:inline text-center">
         SocialBio
       </h1>
       <p className="my-4 text-lg dark:text-white text-gray-600 md:text-xl ">
